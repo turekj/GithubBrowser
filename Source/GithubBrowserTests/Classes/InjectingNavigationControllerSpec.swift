f50e1e779c927f094c4a855d2e8f6307466d4000
Jakub Turek <jkbturek@gmail.com>
2016-09-28 16:44:36 +0200

    Verified Quick dependency works

---
+++ Source/GithubBrowserTests/Classes/InjectingNavigationControllerSpec.swift
@@ -0,0 +1,23 @@
+@testable import GithubBrowser
+import Nimble
+import Quick
+
+
+class InjectingNavigationControllerSpec: QuickSpec {
+    
+    override func spec() {
+        describe("InjectingNavigationController") {
+            context("When navigating forward") {
+                it("Should push resolved view controller on stack") {
+                    fail("Implement this")
+                }
+            }
+            
+            context("When navigating backward") {
+                it("Should pop to last present view controller of class on stack") {
+                    fail("Implement this")
+                }
+            }
+        }
+    }
+}
