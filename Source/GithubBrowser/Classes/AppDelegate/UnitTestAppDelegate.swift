8fa2439c0eddccdc144065cd076eb49e5f92b790
Jakub Turek <jkbturek@gmail.com>
2016-09-28 15:56:26 +0200

    Removed unused code from AppDelegates

--- Source/GithubBrowser/Classes/AppDelegate/UnitTestAppDelegate.swift
+++ Source/GithubBrowser/Classes/AppDelegate/UnitTestAppDelegate.swift
@@ -5,8 +5,8 @@ class UnitTestAppDelegate: NSObject {
     
     var window: UIWindow?
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions
+                     launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         return true
     }
 }
