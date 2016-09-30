d1f387be35233ac4b84822e2300c20798174992d
Jakub Turek <jkbturek@gmail.com>
2016-09-30 12:30:40 +0200

    Renamed imageUrl to url in UserRepository; url is now presented in cell

--- Source/GithubBrowser/Classes/Logic/Mapping/Impl/UserRepositoryFromRepositoryFactory.swift
+++ Source/GithubBrowser/Classes/Logic/Mapping/Impl/UserRepositoryFromRepositoryFactory.swift
@@ -7,7 +7,7 @@ class UserRepositoryFromRepositoryFactory: UserRepositoryFactory {
         return UserRepository(
             id: source.id,
             title: source.name,
-            imageUrl: source.ownerAvatarUrl,
+            url: source.ownerAvatarUrl,
             type: .repository)
     }
 }
--- Source/GithubBrowser/Classes/Model/UserRepository.swift
+++ Source/GithubBrowser/Classes/Model/UserRepository.swift
@@ -5,13 +5,13 @@ struct UserRepository {
     
     let id: Int
     let title: String
-    let imageUrl: String?
+    let url: String?
     let type: UserRepositoryEntryType
     
-    init(id: Int, title: String, imageUrl: String?, type: UserRepositoryEntryType) {
+    init(id: Int, title: String, url: String?, type: UserRepositoryEntryType) {
         self.id = id
         self.title = title
-        self.imageUrl = imageUrl
+        self.url = url
         self.type = type
     }
 }
