9f21cb811d1038a01544b2c7fde624dc8ca8e705
Jakub Turek <jkbturek@gmail.com>
2016-09-30 09:38:41 +0200

    Implemented user detail view stub

--- Source/GithubBrowser/Classes/ViewControllers/UserDetailViewController.swift
+++ Source/GithubBrowser/Classes/ViewControllers/UserDetailViewController.swift
@@ -1,7 +1,37 @@
+import Cartography
 import UIKit
 
 
 class UserDetailViewController: UIViewController, UserDetail {
     
+    let userDetailView: UserDetailView
+    
     var userId: Int?
+    
+    init(view: UserDetailView) {
+        self.userDetailView = view
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.configureView()
+    }
+    
+    func configureView() {
+        self.view.addSubview(self.userDetailView)
+        
+        constrain(self.userDetailView) { v in
+            v.edges == v.superview!.edges
+        }
+    }
+    
+    // MARK: - Required initializer
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
